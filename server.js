a3cd51f560f8a17f301153a040d038be3fc376db
Nawaz Pasha <nawaz22022001@gmail.com>
2023-02-20 12:12:09 +0530

    Added Append API's

--- server.js
+++ server.js
@@ -107,6 +107,83 @@ app.post('/publishResult', upload, async (req, res) => {
 
 });
 
+app.post('/appendResult', upload, async (req, res) => {
+    let result;
+    const schema = {
+        'Register_No': {
+            prop: 'Register_No',
+            type: String
+        },
+        'Cluster': {
+            prop: 'Cluster',
+            type: String
+        },
+        'Name': {
+            prop: 'Name',
+            type: String
+        },
+        'School': {
+            prop: 'School',
+            type: String
+        },
+        'Demonstrating_understanding_of_the_Hauna_vision_and_mission': {
+            prop: 'Demonstrating_understanding_of_the_Hauna_vision_and_mission',
+            type: String
+        },
+        'Demonstrating_knowledge_of_child_and_child_development': {
+            prop: 'Demonstrating_knowledge_of_child_and_child_development',
+            type: String
+        },
+        'Demonstrating_knowledge_of_child_developmental_domains': {
+            prop: 'Demonstrating_knowledge_of_child_developmental_domains',
+            type: String
+        },
+        'Demonstrating_knowledge_of_pedagogical_practices_at_Hauna': {
+            prop: 'Demonstrating_knowledge_of_pedagogical_practices_at_Hauna',
+            type: String
+        },
+        'Demonstrating_knowledge_of_student_assessments': {
+            prop: 'Demonstrating_knowledge_of_student_assessments',
+            type: String
+        },
+        'Demonstrating_Understanding_of_Principles_of_Hauna_Pedagogy': {
+            prop: 'Demonstrating_Understanding_of_Principles_of_Hauna_Pedagogy',
+            type: String
+        },
+        'Pedagogy_Demonstration_Class': {
+            prop: 'Pedagogy_Demonstration_Class',
+            type: String
+        },
+        'Group_Presentation': {
+            prop: 'Group_Presentation',
+            type: String
+        },
+        'Attendance': {
+            prop: 'Attendance',
+            type: String
+        },
+        'Total': {
+            prop: 'Total',
+            type: String
+        },
+        'Total_Percentage': {
+            prop: 'Total_Percentage',
+            type: String
+        },
+    };
+    xlsxFile('./result.xlsx', { schema }).then(async (rows) => {
+        try {
+            result = await db.collection('haunaresult').insertMany([...rows.rows])
+            res.send({
+                'status': 200,
+                'message': 'Result appended successfully',
+                'resultWeGot': result
+            })
+        } catch (error) {
+            console.log('Error', error);
+        }
+    })
+})
 
 app.post('/publishResult2', upload, async (req, res) => {
     let result;
@@ -189,6 +266,83 @@ app.post('/publishResult2', upload, async (req, res) => {
 
 });
 
+app.post('/appendResult2', upload, async (req, res) => {
+    let result;
+    const schema = {
+        'Register_Number': {
+            prop: 'Register_Number',
+            type: String
+        },
+        'Cluster': {
+            prop: 'Cluster',
+            type: String
+        },
+        'Name': {
+            prop: 'Name',
+            type: String
+        },
+        'School': {
+            prop: 'School',
+            type: String
+        },
+        'Demonstrating_Understanding_of_Hauna_Vision_and_Mission': {
+            prop: 'Demonstrating_Understanding_of_Hauna_Vision_and_Mission',
+            type: String
+        },
+        'Demonstrating_Knowledge_of_Child_and_Child_Development': {
+            prop: 'Demonstrating_Knowledge_of_Child_and_Child_Development',
+            type: String
+        },
+        'Demonstrating_Knowledge_of_Child_Developmental_Domains': {
+            prop: 'Demonstrating_Knowledge_of_Child_Developmental_Domains',
+            type: String
+        },
+        'Demonstrating_Knowledge_of_Pedagogical_Practices_at_Hauna': {
+            prop: 'Demonstrating_Knowledge_of_Pedagogical_Practices_at_Hauna',
+            type: String
+        },
+        'Demonstrating_Knowledge_of_Student_Assessments': {
+            prop: 'Demonstrating_Knowledge_of_Student_Assessments',
+            type: String
+        },
+        'Demonstrating_understanding_of_principles_of_Hauna_Pedagogy': {
+            prop: 'Demonstrating_understanding_of_principles_of_Hauna_Pedagogy',
+            type: String
+        },
+        'Pedagogy_Demonstration_Class': {
+            prop: 'Pedagogy_Demonstration_Class',
+            type: String
+        },
+        'Group_Presentation': {
+            prop: 'Group_Presentation',
+            type: String
+        },
+        'Attendance': {
+            prop: 'Attendance',
+            type: String
+        },
+        'Total': {
+            prop: 'Total',
+            type: String
+        },
+        'Percentage': {
+            prop: 'Percentage',
+            type: String
+        },
+    };
+    xlsxFile('./result.xlsx', { schema }).then(async (rows) => {
+        try {
+            result = await db.collection('haunaresult2').insertMany([...rows.rows])
+            res.send({
+                'status': 200,
+                'message': 'Result appended successfully',
+                'resultWeGot': result
+            })
+        } catch (error) {
+            console.log('Error', error);
+        }
+    })
+})
 
 app.delete('/result', async (req, res) => {
     const resultCollection = db.collection('haunaresult');
@@ -224,7 +378,7 @@ app.get('/result', async (req, res) => {
             res.send({
                 'status': 500,
                 'data': [],
-                'message': 'Student with duplicated register no exists'
+                'message': 'User with duplicated register no exists'
             })
         }
         if (result.length == 0) {
@@ -245,7 +399,7 @@ app.get('/result', async (req, res) => {
 
 app.get('/result2', async (req, res) => {
     const result = await db.collection('haunaresult2').find({ 'Register_Number': req.query.reg_no }).toArray();
-    console.log(result)
+    console.log(result.length)
 
     try {
         if (result.length == 1) {
@@ -258,7 +412,7 @@ app.get('/result2', async (req, res) => {
             res.send({
                 'status': 500,
                 'data': [],
-                'message': 'Student with duplicated register no exists'
+                'message': 'User with duplicated register no exists'
             })
         }
         if (result.length == 0) {
